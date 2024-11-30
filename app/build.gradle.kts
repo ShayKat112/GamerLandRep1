043398404b48fb1c2920479b2ae644da117b6f93
ShayKat112 <shaykat1808@gmail.com>
2024-11-30 17:20:29 +0200

    Changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-database:21.0.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
