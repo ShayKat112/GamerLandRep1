06f3c9a140872f54f158014a2615715e21c5dc4b
USER <rotemh4321@gmail.com>
2025-02-23 14:20:41 +0200

    23/2/2025

--- app/src/main/java/com/example/gamerland/ChatCreationActivity.java
+++ app/src/main/java/com/example/gamerland/ChatCreationActivity.java
@@ -1,24 +1,70 @@
 package com.example.gamerland;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class ChatCreationActivity extends AppCompatActivity {
+
+    private Button btnCreateChat;
+    private EditText edChatName, edChatDescription;
+    private FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chat_creation);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnCreateChat = findViewById(R.id.btnCreateChat);
+        edChatName = findViewById(R.id.edChatName);
+        edChatDescription = findViewById(R.id.edChatDescription);
+        db = FirebaseFirestore.getInstance();
+        btnCreateChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage();
+            }
         });
+    }
+
+    private void sendMessage() {
+        String chatName = edChatName.getText().toString();
+        String chatDescription = edChatDescription.getText().toString();
+        if(edChatName.getText().toString().isEmpty() || edChatDescription.getText().toString().isEmpty()){
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+        }else{
+            chatmodel chat = new chatmodel(chatName, chatDescription);
+            db.collection("chats").add(chat).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                @Override
+                public void onSuccess(DocumentReference documentReference) {
+                    Log.d("TAG", "DocumentSnapshot added with ID: " + documentReference.getId());
+                    Toast.makeText(ChatCreationActivity.this, "Chat created", Toast.LENGTH_SHORT).show();
+                    edChatName.setText("");
+                    edChatDescription.setText("");
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.w("TAG", "Error adding document", e);
+                    Toast.makeText(ChatCreationActivity.this, "Error adding document", Toast.LENGTH_SHORT).show();
+                    edChatName.setText("");
+                    edChatDescription.setText("");
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/gamerland/HomeFragment.java
+++ app/src/main/java/com/example/gamerland/HomeFragment.java
@@ -1,12 +1,16 @@
 package com.example.gamerland;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
+    private Button btnCreateChat;
+
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         return fragment;
@@ -23,6 +27,15 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        btnCreateChat = view.findViewById(R.id.btnCreateChat);
+        btnCreateChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ChatCreationActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
