b223d9e2caf2adc4dbb3f1578be21182c11d2523
USER <rotemh4321@gmail.com>
2025-03-23 13:21:39 +0200

    23/3/2025

--- app/src/main/java/com/example/gamerland/SettingsFragment.java
+++ app/src/main/java/com/example/gamerland/SettingsFragment.java
@@ -5,8 +5,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SettingsFragment extends Fragment {
+    private TextView tvUsername;
+    private ImageView tvImvAvatar;
+    private TextView tvAge;
+    private TextView tvEmail;
+    private TextView tvLikedGames;
+    private Button btnChangeProfilePicture;
+    private FirebaseFirestore firestore;
+    private String email;
 
     public static SettingsFragment newInstance() {
         SettingsFragment fragment = new SettingsFragment();
@@ -24,6 +36,9 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        firestore = FirebaseFirestore.getInstance();
+        firestore.collection("users").document(email).get();
+        tvUsername.setText("Username: " + username);
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
 }
